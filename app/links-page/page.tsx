e04868df6833bdf1d41987825950f9bc11d592f9
Aaron Alves <aaronalvesvs@gmail.com>
2026-02-01 18:36:07 -0300

    Add LinksPage component to display user-shared links

---
+++ app/links-page/page.tsx
@@ -0,0 +1,7 @@
+export default function LinksPage() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-semibold text-white"> List of links shared by users.</h1>
+    </div>
+  );
+}
