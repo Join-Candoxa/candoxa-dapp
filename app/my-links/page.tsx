ab1ce644553571e22d38901abddd7141037349c3
Aaron Alves <aaronalvesvs@gmail.com>
2026-02-06 00:42:12 -0300

    Add connection handling and redirect in MyLinksPage component

--- app/my-links/page.tsx
+++ app/my-links/page.tsx
@@ -1,6 +1,20 @@
+"use client"
+
 import DialogRegisterLink from "@/components/DialogRegisterLink";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useConnection } from "wagmi";
 
 export default function MyLinksPage() {
+  const connection = useConnection();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (connection.status === 'disconnected') {
+      router.push('/')
+    }
+  }, [connection.status, router])
+
   return (
     <div className="flex flex-col items-center justify-center gap-7 h-screen">
       <h1 className="text-white italic font-sherika text-3xl text-center">You haven’t registered any links yet. <br /> Start building your identity by saving what matters. <br /> Your reputation grows as others engage.</h1>
